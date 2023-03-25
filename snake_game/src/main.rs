70abf57874a4be4a9104de165060e871c0298251
GaganNayyarHepta <gagan@heptagon.in>
2023-03-25 19:18:58 +0000

    Move and Copy

--- snake_game/src/main.rs
+++ snake_game/src/main.rs
@@ -1,18 +1,14 @@
 
 
 fn main() {
-    
-    let a = 10;
-    let b = a;
-    let _c = 15;
-    let sum = add(a, b);
-
-    println!("{}",sum);
-
+ 
+    let message = String::from("Hello");
+    print_message(message);
 
 }
 
-fn add(x: u32, y:u32) -> u32 {
-    let result = x + y;
-    result
+fn print_message(a: String) {
+    println!("{}",a);
+    let _c = &a;
+    println!("{}", _c)
 }
